a860e2ead339acaa3e11a40130659b71b779da1f
siriusjunior <topdownreading@gmail.com>
2022-10-11 16:06:15 +0900

    [Add]Userのいいねに対応するいいねレイアウト

--- laravel/resources/js/components/ArticleLike.vue
+++ laravel/resources/js/components/ArticleLike.vue
@@ -1,10 +1,30 @@
+<!-- isLikedBy が true である時にハートを⾚く表⽰す -->
 <template>
   <div>
-    <button type="button" class="btn m-0 p-1 shadow-none">
-      <i class="fas fa-heart mr-1"/>
-    </button>10
+    <button
+      type="button"
+      class="btn m-0 p-1 shadow-none"
+    >
+      <i class="fas fa-heart mr-1"
+      :class="{'red-text':this.isLikedBy}"
+      />
+    </button>
+    10
   </div>
 </template>
 
 <script>
+  export default {
+    props: {
+      initialIsLikedBy: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    data() {
+      return {
+        isLikedBy: this.initialIsLikedBy,
+      }
+    },
+  }
 </script>
--- laravel/resources/views/articles/card.blade.php
+++ laravel/resources/views/articles/card.blade.php
@@ -73,7 +73,9 @@
   <!-- ./card-body -->
   <div class="card-body pt-0 pb-2 pl-3">
     <div class="card-text">
-      <article-like>
+      <article-like
+        :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+      >
       </article-like>
       <!-- ArticleLikeではなく、ケバブケースarticle-likeで記述すること -->
     </div>
