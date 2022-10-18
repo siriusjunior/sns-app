0c1db9e1342e82a56044266c5326f0faea55b054
siriusjunior <topdownreading@gmail.com>
2022-10-19 08:58:43 +0900

    [Add]Followボタン・User詳細ページの表示

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
 import ArticleTagsInput from './components/ArticleTagsInput'
+import FollowButton from './components/FollowButton'
 
 // bladeで#app要素の子要素として@yield('content')とあるので各bladeでVueコンポーネントを取り扱える
 const app = new Vue({
@@ -9,5 +10,6 @@ const app = new Vue({
   components: {
     ArticleLike,
     ArticleTagsInput,
+    FollowButton,
   }
 })
