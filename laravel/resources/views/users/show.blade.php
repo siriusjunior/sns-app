47a34176020bfbd654aa0a4613d7f0eb03b52901
siriusjunior <topdownreading@gmail.com>
2022-10-21 16:59:03 +0900

    [Add]userページでフォロワーフォローユーザーページ遷移リンク設置・当該ユーザーのフォローリストページ

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
   @include('nav')
   <div class="container">
     @include('users.user')
-    @include('users.tabs')
+    @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
     @foreach($articles as $article)
       @include('articles.card')
     @endforeach
